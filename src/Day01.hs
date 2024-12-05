c38ed21da183cb125cc83d8d3b7a5d3eaf3cf9cd
Tyler Gallant <tgallant@tucows.com>
2024-12-05 16:01:36 -0500

    day 1 part 2

--- src/Day01.hs
+++ src/Day01.hs
@@ -7,27 +7,40 @@ import Data.Maybe
 import Paths_aoc2024
 import Text.ParserCombinators.ReadP
 
-type Input = [(Int, Int)]
+type Input = ([Int], [Int])
 type Output = Int
 
-parser :: ReadP [(Int, Int)]
-parser = manyTill line eof
+type Solution = Input -> Output
+
+parser :: ReadP Input
+parser = bimap sort sort . unzip <$> manyTill line eof
   where
     line = (,) <$> num <*> num
     num = fmap read $ munch1 isDigit <* skipSpaces
 
-runParser :: ReadP a -> String -> Maybe a
-runParser p = listToMaybe . fmap fst . filter fullParses . readP_to_S p
-  where fullParses = null . snd
+runSolution :: Solution -> String -> Maybe Output
+runSolution solution = processResult . parseAndSolve
+  where
+    fullParses = null . snd
+    parseAndSolve = readP_to_S $ solution <$> parser
+    processResult = listToMaybe . fmap fst . filter fullParses
 
-solution1 :: Input -> Output
+solution1 :: Solution
 solution1 input = sum distances
   where
-    pairs = uncurry zip $ bimap sort sort $ unzip input
+    pairs = uncurry zip input
     distances = abs . uncurry (-) <$> pairs
+
+solution2 :: Solution
+solution2 (listOne, listTwo) = score listOne listTwo
+  where
+    score [] _ = 0
+    score (x:xs) ys = x * countAppearances x ys + score xs ys
+    countAppearances x = length . elemIndices x
 
 day01 :: IO ()
 day01 = do
   inputFileName <- getDataFileName "day01-input.txt"
   input <- readFile inputFileName
-  print $ runParser (solution1 <$> parser) input
+  print $ runSolution solution1 input
+  print $ runSolution solution2 input
