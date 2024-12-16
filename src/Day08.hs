044bcfe2557bfe843bbac6afb33879d61d1ac30e
Tyler Gallant <tylergallant@live.com>
2024-12-16 14:35:06 -0500

    refactor day 8

--- src/Day08.hs
+++ src/Day08.hs
@@ -6,6 +6,7 @@ import Data.Functor
 import Data.List
 import Data.Map.Strict (Map)
 import qualified Data.Map.Strict as Map
+import Data.Maybe
 import Text.ParserCombinators.ReadP hiding (get, many)
 import Utils
 
@@ -15,34 +16,7 @@ type CityMap = Map Pos (Maybe Freq)
 type FreqMap = Map Freq [Pos]
 type Parser = StateT Pos ReadP
 type Input = CityMap
-type Output = FreqMap
-
-onMap :: Pos -> Pos -> Bool
-onMap (mx, my) (x, y) = x >= 0 && y >= 0 && x <= mx && y <= my
-
-antinodes :: Pos -> Pos -> Pos -> [Pos]
-antinodes maxPos (x1, y1) (x2, y2) = onlyOnMap [(x3, y3), (x4, y4)]
-  where
-    diffX = x1 - x2
-    diffY = y1 - y2
-    x3 = x1 - diffX * 2
-    y3 = y1 - diffY * 2
-    x4 = x2 + diffX * 2
-    y4 = y2 + diffY * 2
-    onlyOnMap = filter $ onMap maxPos
-
-antinodes' :: Pos -> Pos -> Pos -> [Pos]
-antinodes' maxPos (x1, y1) (x2, y2)
-  | onMap maxPos (x3, y3) = (x3, y3) : antinodes' maxPos (x2, y2) (x3, y3)
-  where
-    diffX = x2 - x1
-    diffY = y2 - y1
-    x3 = x2 + diffX
-    y3 = y2 + diffY
-antinodes' _ _ _ = []
-
-antinodes'' :: Pos -> Pos -> Pos -> [Pos]
-antinodes'' maxPos p1 p2 = [p1, p2] ++ antinodes' maxPos p1 p2 ++ antinodes' maxPos p2 p1
+type Output = Int
 
 liftParser :: ReadP a -> Parser (Pos, a)
 liftParser p = do
@@ -78,6 +52,26 @@ pCity = Map.fromList . concat <$> pLines
 parser :: ReadP Input
 parser = evalStateT pCity (0, 0)
 
+onMap :: Pos -> Pos -> Bool
+onMap (mx, my) (x, y) = x >= 0 && y >= 0 && x <= mx && y <= my
+
+antinode :: Pos -> Pos -> Pos -> Maybe Pos
+antinode maxPos p1@(x1, y1) p2@(x2, y2)
+  | p1 /= p2 && onMap maxPos p3 = Just p3
+  | otherwise = Nothing
+  where p3 = (x2 + (x2 - x1), y2 + (y2 - y1))
+
+resonantAntinodes :: Pos -> Pos -> Pos -> [Pos]
+resonantAntinodes maxPos p1 p2 = do
+  p3 <- maybeToList $ antinode maxPos p1 p2
+  p3 : resonantAntinodes maxPos p2 p3
+
+allAntinodes :: Pos -> Pos -> Pos -> [Pos]
+allAntinodes maxPos p1 p2 = p1 : p2 : forwardResonants ++ backwardResonants
+  where
+    forwardResonants = resonantAntinodes maxPos p1 p2
+    backwardResonants = resonantAntinodes maxPos p2 p1
+
 freqMap :: CityMap -> FreqMap
 freqMap = Map.foldrWithKey' maybeAddAntenna Map.empty
   where
@@ -89,19 +83,22 @@ freqMap = Map.foldrWithKey' maybeAddAntenna Map.empty
 pairs :: [a] -> [(a, a)]
 pairs xs = [(x,y) | (x:ys) <- tails xs, y <- ys]
 
-reduce :: (Foldable t, Eq a) => Map k (t [a]) -> Int
-reduce = length . nub . concat . Map.elems . fmap concat
-
 getMaxPos :: Map Pos a -> Pos
 getMaxPos = maybe (0, 0) fst . Map.lookupMax
 
-solution1 :: Solution Input Int
-solution1 city = reduce $ fmap getAntinodes . pairs <$> freqMap city
-  where getAntinodes = uncurry . antinodes $ getMaxPos city
+reduce :: (Foldable t, Eq a) => Map k (t [a]) -> Int
+reduce = length . nub . concat . Map.elems . fmap concat
 
-solution2 :: Solution Input Int
+solution1 :: Solution Input Output
+solution1 city = reduce $ fmap getAntinodes . pairs <$> freqMap city
+  where
+    getAntinodes (p1, p2) = catMaybes [getAntinode p1 p2, getAntinode p2 p1]
+    getAntinode = antinode maxPos
+    maxPos = getMaxPos city
+
+solution2 :: Solution Input Output
 solution2 city = reduce $ fmap getAntinodes . pairs <$> freqMap city
-  where getAntinodes = uncurry . antinodes'' $ getMaxPos city
+  where getAntinodes = uncurry . allAntinodes $ getMaxPos city
 
 day08 :: IO ()
 day08 = do
